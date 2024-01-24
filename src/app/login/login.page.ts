4f20620d9b483ad489f66fb8e8991ff45b5b87f9
DarwinBalza <djosue1309@gmail.com>
2024-01-23 22:37:26 -0500

    clase 23-01-2024

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+  loginForm: FormGroup;
+  constructor(private formBiulder: FormBuilder) { 
+    this.loginForm = this.formBiulder.group({
+      email: new FormControl(
+        "",
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(
+            "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$"
+          )
+        ]) 
+      )
+    })
+  }
+
+  ngOnInit() {
+  }
+
+  login(login_data: any){
+    console.log(login_data)
+  }
+
+}
